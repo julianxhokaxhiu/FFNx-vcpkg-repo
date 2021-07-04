ca20e5cfa8747c7ea1320ecc560129c570734667
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2021-07-04 15:20:48 +0200

    Fix openpsf build and inclusion

--- ports/openpsf/portfile.cmake
+++ ports/openpsf/portfile.cmake
@@ -4,18 +4,18 @@
 
 vcpkg_from_github(
     OUT_SOURCE_PATH HE_SOURCE_DIR
-    REPO "myst6re/highly_experimental"
+    REPO "julianxhokaxhiu/highly_experimental"
     HEAD_REF master
-    REF 0768ebd43e7be05786e67d346091117457ddab2b
-    SHA512 7385770b2e834c36a2b53fb02cde099faf7e1a77443b6bb66dfb1db71cfee7d4cf4fb4588a266472821829fa4d511871a7f6205de7c3cb956a51fcd95c237a26
+    REF 9790e0bf726ac7f3120f860dfcbb550003df45f6
+    SHA512 e3943edbbffde2e0c5e170755beb57043b6d1f78464c40766af7a5c1d6a0d91a80b8466a75e421d87271b10fffcc24a40950290f9b1f901f506963545da1676a
 )
 
 vcpkg_from_github(
     OUT_SOURCE_PATH PSFLIB_SOURCE_DIR
     REPO "julianxhokaxhiu/psflib"
     HEAD_REF master
-    REF 6d276bc19c49d3913f5bac2b85f2c5566ff7fea4
-    SHA512 feba0b96f79284c7863f478098bd245008c0d7cabc418034d53b0200850811fbe2cd73fa095813156edf602a292c7a662356c82270d0acb5a943a9b4a0ff2e44
+    REF b74d6e8e9fcf0d3a5ebb147f036d80067ee09cb8
+    SHA512 19f53f4d8519c343b7f2a2acf6315daf00938a59bd9366a989db0845fdb1307638e1b5d0ae57bd72fef4e586952d3a58369ae8b9914e52a29fe773a80b00c083
 )
 
 # Checkout this project
@@ -24,8 +24,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_DIR
     REPO "julianxhokaxhiu/openpsf"
     HEAD_REF master
-    REF 71e747a217fd1f7e396bb824f5a925f34fcf4990
-    SHA512 a4878ad3f54f35633191e717ac8f74e69848cdfbb2b6e46f3e5b1c4a36edcd1772d18f183132c8e55c9b28e69daafbabfe0116bd18ac9e3dff19b31310e66069
+    REF cdee811929fd76c9938471bc41a388c315cd187b
+    SHA512 603a9ad6e918563c9358f92907c51f0a3c8caf48789d23e3d64e5ff9251a9a228c830d8de9d402237634daad0223e612168efeff9f9472de0591914098e6a32d
 )
 
 # Move dependencies inside the project directory
@@ -36,12 +36,12 @@ file(RENAME ${PSFLIB_SOURCE_DIR} "${SOURCE_DIR}/psflib")
 
 vcpkg_install_msbuild(
     SOURCE_PATH "${SOURCE_DIR}"
-    PROJECT_SUBPATH "openpsf.vcxproj"
+    PROJECT_SUBPATH "openpsf.sln"
     LICENSE_SUBPATH "LICENSE"
-    INCLUDES_SUBPATH "src"
-    ALLOW_ROOT_INCLUDES
+    INCLUDES_SUBPATH "include"
     USE_VCPKG_INTEGRATION
 )
 
-# Copy psflib.h header
-file(COPY "${SOURCE_DIR}/psflib/psflib.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+# Copy dependencies headers
+file(INSTALL "${SOURCE_DIR}/highly_experimental/include" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(INSTALL "${SOURCE_DIR}/psflib/include" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
